62d16b978173e6c6dd462925074465205c7d2f64
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-01-23 20:32:33 +0200

    Add files via upload

---
+++ Cars/2301Cars/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2301Cars
+{
+    class Car
+    {
+        // --------------------------------------- DATA
+        public int year;
+        public double km;
+        public string brand;
+        public string model;
+
+
+        // --------------------------------------- FUNCTIONS
+        public string TellMeYourModel()
+        {
+            return model;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine($"Driving {brand} {model}");
+        }
+
+        public void Start()
+        {
+            Console.WriteLine($"Starting {brand} {model} {km}");
+        }
+
+        public double TellMeYourKm()
+        {
+            return km;
+        }
+    }
+}
---
+++ Cars/2301Cars/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2301Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car lamburguini = new Car
+            {
+                brand = "Lamburgini",
+                model = "Hurikan",
+                km = 100,
+                year = 2020
+            };
+
+            Car ferrari = new Car
+            {
+                brand = "Ferrari",
+                model = "Range",
+                km = 50.5,
+                year = 2021
+            };
+
+            Car mitsubishi = new Car
+            {
+                brand = "Mitsubishi",
+                model = "Outlander",
+                km = 10002.4,
+                year = 2017
+            };
+
+            lamburguini.Start();
+            lamburguini.Drive();
+
+            Console.WriteLine( ferrari.TellMeYourKm() );
+            Console.WriteLine( mitsubishi.TellMeYourModel() );
+        }
+    }
+}
